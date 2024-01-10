f738a7ed109195e82652c00a10baed513f9ca70d
Myrsini Gkolemi <mirtoseri@gmail.com>
2024-01-10 14:37:11 +0100

    wow

--- src/jacobi.c
+++ src/jacobi.c
@@ -7,35 +7,32 @@
     (-2.0 / 3) <= z && z <= 0)                                                 \
        ? 200                                                                   \
        : 0)
+#define pow2(x) x*x
+
 
 int jacobi(double ***u_curr, double ***u_prev, double ***f, int N,
            int max_iterations, double tolerance) {
   // It's up to us if we return iterations or some error indicating value
   int iter = 0;
   double delta = 1.0 / N;
-
+  double distance = 0;
   do {
-    for (int i = 1; i < N; i++) {
-      for (int j = 1; j < N; j++) {
+    for (int i = 1; i < N; i++)
+      for (int j = 1; j < N; j++)
         for (int k = 1; k < N; k++) {
           u_curr[i][j][k] =
               (1.0 / 6) *
               (u_prev[i - 1][j][k] + u_prev[i + 1][j][k] + u_prev[i][j - 1][k] +
                u_prev[i][j + 1][k] + u_prev[i][j][k - 1] + u_prev[i][j][k + 1] +
-               delta * delta * f[i][j][k]);
-        }
-      }
-    }
-    // calculate distance
-
-    // coppy u_curr to u_prev
-    for (int i = 1; i < N; i++)
-      for (int j = 1; j < N; j++)
-        for (int k = 1; k < N; k++)
+               pow2(delta) * f[i][j][k]);
+          // update here
+          distance += pow2(fabs(u_prev[i][j][k] - u_curr[i][j][k]));
+          // copy
           u_prev[i][j][k] = u_curr[i][j][k];
-
+        }
     ++iter;
-  } while (iter < max_iterations && /* tolerance check */);
+    distance = sqrt(distance);
+  } while (iter < max_iterations && distance < tolerance);
 
   return iter;
 }
