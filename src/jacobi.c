f79c3a412888279863f01c9fed149266d008d31a
fverne <frederikverne95@gmail.com>
2024-01-10 16:42:46 +0100

    fixed distance

--- src/jacobi.c
+++ src/jacobi.c
@@ -8,8 +8,9 @@ int jacobi(double ***u_curr, double ***u_prev, double ***f, int N,
            int max_iterations, double tolerance) {
   int iter = 0;
   double delta = 1.0 / (N - 1);
-  double distance = 0;
+  double distance;
   do {
+    distance = 0;
     for (int i = 1; i < N - 1; i++)
       for (int j = 1; j < N - 1; j++)
         for (int k = 1; k < N - 1; k++) {
