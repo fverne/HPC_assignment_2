7f24cdc9f4cd3c26b596d6c3b34874e8a7d3e850
Myrsini Gkolemi <mirtoseri@gmail.com>
2024-01-11 13:51:25 +0100

    script fix

--- run.sh
+++ run.sh
@@ -23,25 +23,25 @@ GRID_VALUES=(
 # go to source dir
 cd "src"
 # create logs dir if doesn't exist
+rm -rf "logs"
 mkdir -p "logs"
 # remove previous logs
-rm -f "logs/*.log"
 
 # no real meaning behind these values for now
 ITERATIONS_DEF=10000
-TOLERANCE_DEF=0.00001
+TOLERANCE_DEF=0.0001
 GRID_DEF=50
 
 for GRID in "${GRID_VALUES[@]}"; do
-  ./poisson_j $GRID $ITERATIONS_DEF $TOLERANCE_DEF 0.0 4 >>"logs/grid_j.log"
   ./poisson_gs $GRID $ITERATIONS_DEF $TOLERANCE_DEF 0.0 4 >>"logs/grid_gs.log"
+  ./poisson_j $GRID $ITERATIONS_DEF $TOLERANCE_DEF 0.0 4 >>"logs/grid_j.log"
   ./poisson_j $GRID $ITERATIONS_DEF $TOLERANCE_DEF 0.0 3 >/dev/null 2>&1
   ./poisson_gs $GRID $ITERATIONS_DEF $TOLERANCE_DEF 0.0 3 >/dev/null 2>&1
 done
 
 for TOLERANCE in "${TOLERANCE_VALUES[@]}"; do
-  ./poisson_j  $GRID_DEF $ITERATIONS_DEF $TOLERANCE 0.0 4 >>"logs/tolerance_j.log"
   ./poisson_gs $GRID_DEF $ITERATIONS_DEF $TOLERANCE 0.0 4 >>"logs/tolerance_gs.log"
+  ./poisson_j  $GRID_DEF $ITERATIONS_DEF $TOLERANCE 0.0 4 >>"logs/tolerance_j.log"
   ./poisson_gs $GRID_DEF $ITERATIONS_DEF $TOLERANCE 0.0 3 >/dev/null 2>&1
   ./poisson_gs $GRID_DEF $ITERATIONS_DEF $TOLERANCE 0.0 3 >/dev/null 2>&1
 done
@@ -61,8 +61,8 @@ mv $(fd .zip) "$(pwd)output/zip"
 mv $(fd .bin) "$(pwd)/output/bin"
 mv $(fd .vtk) "$(pwd)/output/vtk"
 
-# check sequential with bin version!
-# UNIQ=$(md5sum *.bin | uniq -c | wc -l)
-# ALL=$(ls *.bin | wc -l)
-# echo -e "Unique files: ${UNIQ}"
-# echo -e "Total files: ${ALL}"
+check sequential with bin version!
+UNIQ=$(md5sum *.bin | uniq -c | wc -l)
+ALL=$(ls *.bin | wc -l)
+echo -e "Unique files: ${UNIQ}"
+echo -e "Total files: ${ALL}"
