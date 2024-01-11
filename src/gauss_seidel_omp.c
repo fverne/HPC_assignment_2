0ba7dd3a14f11150a07518ddf1386f1f18d66fa7
fverne <frederikverne95@gmail.com>
2024-01-11 08:57:18 +0100

    added makefile for gsomp

--- src/gauss_seidel_omp.c
+++ src/gauss_seidel_omp.c
@@ -1,10 +1,36 @@
-/* gauss_seidel.c - Poisson problem in 3d with omp
+/* gauss_seidel_omp.c - Poisson problem in 3d with omp
  *
  */
 #include <math.h>
+#include <omp.h>
+#include "gauss_seidel_omp.h"
 
-void
-gauss_seidel_omp() {
-    // fill in your omp code here
+int gauss_seidel_omp(double ***u_curr, double ***u_prev, double ***f, int N,
+                 int max_iterations, double tolerance) {
+  int iter = 0;
+  double delta = 2.0 / (N - 1);
+  double fraction = (1.0 / 6);
+  double delta_2 = pow2(delta);
+  double distance;
+  do {
+    distance = 0;
+    for (int i = 1; i < N - 1; i++)
+      for (int j = 1; j < N - 1; j++)
+        for (int k = 1; k < N - 1; k++) {
+          u_curr[i][j][k] =
+              fraction *
+              (u_curr[i - 1][j][k] + u_prev[i + 1][j][k] + u_curr[i][j -
+              1][k] +
+               u_prev[i][j + 1][k] + u_curr[i][j][k - 1] + u_prev[i][j][k +
+               1] + delta_2 * f[i][j][k]);
+          // distance
+          distance += pow2(u_prev[i][j][k] - u_curr[i][j][k]);
+          // copy
+          u_prev[i][j][k] = u_curr[i][j][k];
+        }
+    ++iter;
+    distance = sqrt(distance);
+  } while (iter < max_iterations && distance > tolerance);
+
+  return iter;
 }
-
--- src/gauss_seidel_omp.h
+++ src/gauss_seidel_omp.h
@@ -3,8 +3,9 @@
  */
 #ifndef _GAUSS_SEIDEL_OMP_H
 #define _GAUSS_SEIDEL_OMP_H
+#include "util.h"
 
-// define your function prototype here
-// gauss_seidel_omp(...);
+int gauss_seidel_omp(double ***u_curr, double ***u_prev, double ***f, int N,
+                 int max_iterations, double tolerance);
 
 #endif
