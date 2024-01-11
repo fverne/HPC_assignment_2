68eb27e4fdbc232faeb7d0a082be02ad2c8355c5
Chang-Rong Xue <changrong.xue@hotmail.com>
2024-01-11 15:48:15 +0100

    func(jaboci_omp): draft implementation of expanded parall-region
    It's buggy!

--- src/jacobi_omp.c
+++ src/jacobi_omp.c
@@ -13,29 +13,35 @@ int jacobi_omp(
   double delta = 2.0 / (N - 1);
   double distance;
   double t;
-  do {
-    distance = 0;
-    #pragma omp parallel for private(t) reduction(+:distance) 
-    for (int i = 1; i < N - 1; i++)
-      for (int j = 1; j < N - 1; j++)
-        for (int k = 1; k < N - 1; k++) {
-          double update_value =
-              (1.0 / 6) *
-              (u_prev[i - 1][j][k] + u_prev[i + 1][j][k] + u_prev[i][j - 1][k] +
-               u_prev[i][j + 1][k] + u_prev[i][j][k - 1] + u_prev[i][j][k + 1] +
-               pow2(delta) * f[i][j][k]);
-          u_curr[i][j][k] = update_value;
-          t = pow2(u_prev[i][j][k] - update_value);
-          distance += t;
-    } // Parallel region ends here
+  #pragma omp parallel private(t) 
+  {
+    do {
+      distance = 0;
+      #pragma omp for reduction(+:distance) 
+      for (int i = 1; i < N - 1; i++)
+        for (int j = 1; j < N - 1; j++)
+          for (int k = 1; k < N - 1; k++) {
+            double update_value =
+                (1.0 / 6) *
+                (u_prev[i - 1][j][k] + u_prev[i + 1][j][k] + u_prev[i][j - 1][k] +
+                 u_prev[i][j + 1][k] + u_prev[i][j][k - 1] + u_prev[i][j][k + 1] +
+                 pow2(delta) * f[i][j][k]);
+            u_curr[i][j][k] = update_value;
+            t = pow2(u_prev[i][j][k] - update_value);
+            distance += t;
+      } // Parallel region ends here
 
-    double ***tmp = u_prev;           
-    u_prev = u_curr;
-    u_curr = tmp;
-
-    ++iter;
-    distance = sqrt(distance);
-  } while (iter < iter_max && distance > tolerance);
+      #pragma omp single 
+      {
+        double ***tmp = u_prev;           
+        u_prev = u_curr;
+        u_curr = tmp;
+        
+        ++iter;
+        distance = sqrt(distance);
+      }
+    } while (iter < iter_max && distance > tolerance);
+  }
 
   // check the odd/even of iterations
   if (iter % 2 == 0) {
