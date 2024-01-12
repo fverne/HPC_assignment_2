bae4372ff62b44704ec9aff283cf6df1be02a42b
fverne <frederikverne95@gmail.com>
2024-01-12 02:34:44 +0100

    small mistake to convertdata

--- convertdata.py
+++ convertdata.py
@@ -9,7 +9,7 @@ filenames= [
     ]
 
 for filename in filenames:
-    n = [re.findall(r'(?<=N:(\s)+)(\d+)\w+',line) 
+    n = [re.findall(r'(?<=N:(\s)+)(\d+)\w*',line) 
             for line in open(f'src/logs/{filename}.log')]
 
     time = [re.findall(r'(?<=Time:(\s)+)(\d+\.\d+)\w+',line) 
